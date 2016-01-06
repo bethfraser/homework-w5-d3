850dab5e9fe2a7b1104bcbc8318186e23609e50a
Beth Fraser <bethanyfraser@live.co.uk>
2016-01-06 14:37:16 +0000

    Markdown added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  def html
+    renderer = Redcarpet::Render::HTML.new(render_options = {})
+    markdown = Redcarpet::Markdown.new(renderer, extensions = {})
+    markdown.render(self.content).html_safe
+  end
 end
